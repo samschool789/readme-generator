1461b7cb7ffe13a9461f77a86f31c01290734499
samschool789@gmail.com <samschool789@gmail.com>
2022-05-14 22:00:08 -0500

    Write File

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
-//const generateMarkdown = require('./utils/generateMarkdown.js');
+//const axios = require("axios");
+const generate = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 
-const promptUser = () => {
-  return inquirer.prompt([
+const questions = [
     {
       type: "input",
       message: "What is your GitHub username?",
@@ -28,26 +28,68 @@ const promptUser = () => {
       name: "tableOfContents",
       message: "Please provide a Table of Contents",
     },
-  ]);
-};
-promptUser().then((answers) => console.log(answers));
+
+    {
+        type: 'input',
+        message: "If applicable, describe the steps required to install your project for the Installation section.",
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: "Provide instructions and examples of your project in use for the Usage section.",
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: "If applicable, provide guidelines on how other developers can contribute to your project.",
+        name: 'contributing'
+    },
+    {
+        type: 'input',
+        message: "If applicable, provide any tests written for your application and provide examples on how to run them.",
+        name: 'tests'
+    },
+    {
+        type: 'list',
+        message: "Choose a license for your project.",
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+        name: 'license'
+    }
+];
+
+
+ inquirer
+  .prompt(questions)
+  .then(function(data){
+     
+        fs.writeFile("./README.md", generate(data, ), function(err) {
+          if (err) {
+            throw err;
+          };
+  
+          console.log("New README file created with success!");
+        });
+      });
 
 
 
 
-
-
-
-
-// fs.writeFile('README.md', generateMarkdown(input, data), err => {
-//     if (err) throw err;
-//    console.log('success! your README was generated!')
-// });
-
-// TODO: Create a function to write README file
+  // TODO: Create a function to write README file
 
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
 init();
+
+
+ 
+
+
+
+
+
+
+
+
+
