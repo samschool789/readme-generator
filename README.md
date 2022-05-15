21fbd6265cec28455eda33c5540893e9d2379d7b
samschool789@gmail.com <samschool789@gmail.com>
2022-05-15 16:35:54 -0500

    changes to code

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# undefined
+# f
+  ![Github licence](http://img.shields.io/badge/license-MIT-blue.svg)
+  
+  ## Description 
+  f
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation 
+  f
+  ## Usage 
+  f
+  ## License 
+  This project is license under MIT
+  ## Contributing 
+  f
+  ## Tests
+  f
+  ## Questions
+  If you have any questions about this projects, please contact me directly at f. You can view more of my projects at https://github.com/f.
--- index.js
+++ index.js
@@ -1,92 +1,150 @@
-// TODO: Include packages needed for this application
-const fs = require("fs");
-const inquirer = require("inquirer");
-//const axios = require("axios");
-const generate = require('./utils/generateMarkdown');
+// require modules 
+const fs = require('fs'); 
+const inquirer = require('inquirer'); 
 
-// TODO: Create an array of questions for user input
+// linking to page where the README is developed 
+const generatePage = require('./utils/generateMarkdown.js');
 
-const questions = [
-    {
-      type: "input",
-      message: "What is your GitHub username?",
-      name: "username",
-    },
-
-    {
-      type: "input",
-      message: "What is the title of your project?",
-      name: "projectName",
-    },
-    {
-      type: "input",
-      name: "description",
-      message: "Please describe your project?",
-    },
-    {
-      type: "input",
-      name: "tableOfContents",
-      message: "Please provide a Table of Contents",
-    },
-
+// array of questions for user
+const questions = () => {
+    // using inquirer to prompt questions to user 
+    return inquirer.prompt([
     {
         type: 'input',
-        message: "If applicable, describe the steps required to install your project for the Installation section.",
-        name: 'installation'
+        name: 'github',
+        message: 'What is your GitHub username?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username!');
+                return false; 
+            }
+        } 
     },
     {
         type: 'input',
-        message: "Provide instructions and examples of your project in use for the Usage section.",
-        name: 'usage'
+        name: 'email',
+        message: 'What is your email address?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your email address!');
+                return false; 
+            }
+        }
+
     },
     {
         type: 'input',
-        message: "If applicable, provide guidelines on how other developers can contribute to your project.",
-        name: 'contributing'
+        name: 'title',
+        message: 'What is your project name?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your project name!');
+                return false; 
+            }
+        }
     },
     {
         type: 'input',
-        message: "If applicable, provide any tests written for your application and provide examples on how to run them.",
-        name: 'tests'
-    },
+        name: 'description',
+        message: 'Please write a short description of your project.',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter a description of your project!');
+                return false; 
+            }
+        }
+    }, 
     {
         type: 'list',
-        message: "Choose a license for your project.",
-        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
-        name: 'license'
+        name: 'license',
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'GNU'],
+        default: ["MIT"],
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please choose a license!');
+                return false; 
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'What are the steps required to install your project?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter steps required to install your project!');
+                return false; 
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use this app?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter a usage description!');
+                return false; 
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'test', 
+        message: 'What command should be run to run tests?',
+        default: 'npm test'
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'What does the user need to know about contributing to the repo?'
     }
-];
+]);
+};
 
+// function to write README file using file system 
+const writeFile = data => {
+    fs.writeFile('README.md', data, err => {
+        // if there is an error 
+        if (err) {
+            console.log(err);
+            return;
+        // when the README has been created 
+        } else {
+            console.log("Your README has been successfully created!")
+        }
+    })
+}; 
 
- inquirer
-  .prompt(questions)
-  .then(function(data){
-     
-        fs.writeFile("./README.md", generate(data, ), function(err) {
-          if (err) {
-            throw err;
-          };
-  
-          console.log("New README file created with success!");
-        });
-      });
-
-
-
-
-  // TODO: Create a function to write README file
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
-
-
- 
-
-
-
+// function call to initialize program
+questions()
+// getting user answers 
+.then(answers => {
+    return generatePage(answers);
+})
+// using data to display on page 
+.then(data => {
+    return writeFile(data);
+})
+// catching errors 
+.catch(err => {
+    console.log(err)
+})
 
 
 
